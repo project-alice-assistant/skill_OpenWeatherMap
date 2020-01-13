d1ad75ce05a84125c9d03c3a9102980e2f6c52ea
Psychokiller1888 <laurentchervet@bluewin.ch>
2020-01-13 18:46:53 +0100

    missing owm

--- widgets/CurrentWeather.py
+++ widgets/CurrentWeather.py
@@ -1,11 +1,12 @@
 import sqlite3
 
 from core.base.model.Widget import Widget
+from core.base.model.widgetSizes import WidgetSizes
 
 
 class CurrentWeather(Widget):
 
-	SIZE = 'w_small'
+	SIZE = WidgetSizes.w_small
 	OPTIONS = dict()
 
 	def __init__(self, data: sqlite3.Row):
